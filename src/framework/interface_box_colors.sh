05e0463bcc75bd39906f070bb3b4ca593cce5021
tearran <tearran@gmail.com>
2025-06-05 00:21:01 +0000

    fix style

--- src/framework/interface_box_colors.sh
+++ src/framework/interface_box_colors.sh
@@ -53,44 +53,47 @@ _unset_interface_box_colors() {
 }
 
 _newt_colors() {
-    local color_code="$1"
-    local color
-    case "$color_code" in
-        0) color="black" ;;
-        1) color="red" ;;
-        2) color="green" ;;
-        3) color="yellow" ;;
-        4) color="blue" ;;
-        5) color="magenta" ;;
-        6) color="cyan" ;;
-        7) color="white" ;;
-        8) color="black" ;;
-        9) color="red" ;;
-        *) return 1 ;;
-    esac
-    export NEWT_COLORS="root=,$color"
+
+	local color_code="$1"
+	local color
+	case "$color_code" in
+		0) color="black" ;;
+		1) color="red" ;;
+		2) color="green" ;;
+		3) color="yellow" ;;
+		4) color="blue" ;;
+		5) color="magenta" ;;
+		6) color="cyan" ;;
+		7) color="white" ;;
+		8) color="black" ;;
+		9) color="red" ;;
+		*) return 1 ;;
+	esac
+	export NEWT_COLORS="root=,$color"
+
 }
 
 _term_colors() {
-    local color_code="$1"
-    local color
-    case "$color_code" in
-        0) color="\e[40m" ;; # black
-        1) color="\e[41m" ;; # red
-        2) color="\e[42m" ;; # green
-        3) color="\e[43m" ;; # yellow
-        4) color="\e[44m" ;; # blue
-        5) color="\e[45m" ;; # magenta
-        6) color="\e[46m" ;; # cyan
-        7) color="\e[47m" ;; # white
-        8) color="\e[100m" ;; # bright black / gray
-        9) color="\e[101m" ;; # bright red
-        *)
-            echo "Invalid color code"
-            return 1
-            ;;
-    esac
-    echo -e "$color"
+
+	local color_code="$1"
+	local color
+	case "$color_code" in
+		0) color="\e[40m" ;; # black
+		1) color="\e[41m" ;; # red
+		2) color="\e[42m" ;; # green
+		3) color="\e[43m" ;; # yellow
+		4) color="\e[44m" ;; # blue
+		5) color="\e[45m" ;; # magenta
+		6) color="\e[46m" ;; # cyan
+		7) color="\e[47m" ;; # white
+		8) color="\e[100m" ;; # bright black / gray
+		9) color="\e[101m" ;; # bright red
+		*)
+		echo "Invalid color code"
+		return 1
+		;;
+	esac
+	echo -e "$color"
 }
 
 _about_interface_box_colors() {
