39ea5dcb26fedd23537a96900f01b9d9e28386e4
tearran <tearran@gmail.com>
2025-06-05 03:32:21 +0000

    migrated menu

---
+++ src/framework/interface_message_box.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+function interface_message() {
+	# Check if argument is provided
+	if [[ -n "$1" ]]; then
+		input="$1"
+	else
+		input=$(cat)
+	fi
+	# Display menu based on DIALOG tool
+	case $DIALOG in
+		"dialog")
+		$DIALOG --title "$TITLE" --msgbox "$input" 0 0 2>&1 >/dev/tty
+		;;
+		"whiptail")
+		whiptail --title "$TITLE" --msgbox "$input" 0 0 3>&1 1>&2 2>&3
+		;;
+		"read")
+		echo "Available options:"
+		;;
+	esac
+}
+
+# for termnal use
+# interface_message "$@"
+
+# fpr pipes
+# echo "string" | interface_message
+# interface_message <<< "hello"
