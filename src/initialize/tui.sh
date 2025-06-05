7e813057f1cf65262da53a69b25a497089c0caa9
tearran <tearran@gmail.com>
2025-06-05 20:03:55 +0000

    new file:   src/initialize/tui.sh

---
+++ src/initialize/tui.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+initialize_interface() {
+	local options="$1"
+
+	case "$options" in
+
+		help)
+		_about_initialize_interface
+		;;
+		*)
+		# Set dialog tool hierarchy based on environment
+		if [[ -x "$(command -v whiptail)" ]]; then
+			DIALOG="whiptail"
+		elif [[ -x "$(command -v dialog)" ]]; then
+			DIALOG="dialog"
+		else
+			die "TUI unavailable"
+		fi
+		checkpoint debug "Initialized (TUI) text user interface is ($DIALOG)..."
+		;;
+	esac
+
+}
+
+
+function _about_initialize_interface() {
+	cat <<EOF
+Usage: initialize_interface
+
+EOF
+
+}
+
