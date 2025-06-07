394457aa6a076d6177e115b6b9e8d249ca1d4b53
tearran <tearran@gmail.com>
2025-06-07 19:28:54 +0000

    Test case

--- testing/module_docgen.sh
+++ testing/module_docgen.sh
@@ -7,6 +7,6 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 	source "$script_dir/../lib/module_options_arrays.sh"
 	source "$script_dir/../src/framework/metadata_array_parsing.sh"
-	_merge_metadata_arrays framework_options framework_helpers
-	_metadata_md_table module_options > "$script_dir/../docs/metadata_tables.md"
+	source "$script_dir/../src/docgen/module_docgen.sh"
+	module_docgen
 fi
