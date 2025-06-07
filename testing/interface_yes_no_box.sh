1bd4573b1eb46e6bc5747ac24dc5437cd94c3af9
tearran <tearran@gmail.com>
2025-06-07 18:16:05 +0000

    modified:   testing/interface_yes_no_box.sh

--- testing/interface_yes_no_box.sh
+++ testing/interface_yes_no_box.sh
@@ -2,10 +2,10 @@
 
 # shellcheck disable=SC2034
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    source "$script_dir/../lib/module_options_arrays.sh"
-    source "$script_dir/../src/framework/interface_yes_no_box.sh"
+	script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	source "$script_dir/../lib/module_options_arrays.sh"
+	source "$script_dir/../src/framework/interface_yes_no_box.sh"
 
-    DIALOG="${DIALOG:-whiptail}"
-    interface_yes_no "Do you want to continue?" _process_input
+	DIALOG="${DIALOG:-whiptail}"
+	interface_yes_no "Do you want to continue?" _process_input
 fi
