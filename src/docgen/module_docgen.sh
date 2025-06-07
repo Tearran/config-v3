fd7d9f7f660bfaaddfa0818f7e68f0ebf2cffac6
tearran <tearran@gmail.com>
2025-06-07 19:30:43 +0000

    style fix

--- src/docgen/module_docgen.sh
+++ src/docgen/module_docgen.sh
@@ -1,14 +1,14 @@
 module_docgen() {
-    case "$1" in
-        md|markdown)
-            _metadata_md_table module_options > doc.md
-            ;;
-        help)
-            _metadata_help module_options
-            ;;
-        *)
-            echo "Usage: module_docgen {md|help}" >&2
-            return 1
-            ;;
-    esac
+	case "$1" in
+		md|markdown)
+			_metadata_md_table module_options > doc.md
+		;;
+		help)
+			_metadata_help module_options
+		;;
+		*)
+			echo "Usage: module_docgen {md|help}" >&2
+			return 1
+		;;
+	esac
 }
