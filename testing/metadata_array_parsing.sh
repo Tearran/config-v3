f9c22dbc17cf3bed241317253c5c784ad577e0cd
Joey Turner <Tearran@users.noreply.github.com>
2025-06-07 19:03:58 -0700

    add Bash tests for metadata and dialog parsing (#19)
    * renamed:    testing/manual/metadata_array_parsing.sh -> src/framework/metadata_array_parsing.sh
    new file:   testing/interface_yes_no_box.sh
    new file:   testing/metadata_array_parsing.sh
    
    * modified:   testing/interface_yes_no_box.sh
    
    * Test case
    
    * style fix
    
    * shellcheck fix
    
    * shabang
    
    * message edit
    
    * new file:   doc.md
    	modified:   src/docgen/module_docgen.sh
    	modified:   testing/interface_yes_no_box.sh
    	modified:   testing/metadata_array_parsing.sh
    	modified:   testing/module_docgen.sh

---
+++ testing/metadata_array_parsing.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+declare -A module_options
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+	script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	source "$script_dir/../lib/module_options_arrays.sh"
+	source "$script_dir/../src/framework/metadata_array_parsing.sh"
+	_merge_metadata_arrays framework_options framework_helpers
+	_metadata_md_table module_options > "$script_dir/../docs/metadata_tables.md"
+fi
