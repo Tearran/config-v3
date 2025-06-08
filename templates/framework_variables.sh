9903b7135888c098b112d7414694a109cf1a5768
tearran <tearran@gmail.com>
2025-06-08 05:37:43 +0000

    deleted:    src/framework/framework_variables.conf 	deleted:    src/framework/framework_variables.sh 	new file:   templates/framework_variables.sh 	renamed:    templates/module_skeleton.conf -> templates/module_template.conf

---
+++ templates/framework_variables.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Template for framework variables in entrypoint binaries
+
+
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
+TOOLS_DIR="${ROOT}/tools"
+# Add more exports as required...
+
+
+_unset_paths() {
+
+	unset SCRIPT_PATH
+	unset SCRIPT_NAME
+	unset SCRIPT_DIR
+	unset ROOT
+	unset TOOLS_DIR
+}
+
