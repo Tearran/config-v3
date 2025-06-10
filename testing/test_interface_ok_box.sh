6b24c5d5931258de182651fdb3746e02310f6e25
tearran <tearran@gmail.com>
2025-06-10 04:32:49 +0000

    fix naming

--- testing/test_interface_ok_box.sh
+++ testing/test_interface_ok_box.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 # Example usage: Only run if called directly, not sourced
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	DIALOG="${DIALOG:-whiptail}"
-	source ./src/framework/interface_message_box.sh
-
+	source ./src/framework/interface_ok_box.sh
 	interface_ok_box "${1:-help}"
 fi
