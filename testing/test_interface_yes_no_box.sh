1cd9fb78b65c12f79ebfb13714fcbcaf87151f64
tearran <tearran@gmail.com>
2025-06-10 04:28:21 +0000

    fix testing

--- testing/test_interface_yes_no_box.sh
+++ testing/test_interface_yes_no_box.sh
@@ -6,5 +6,5 @@ if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
 	DIALOG="${DIALOG:-whiptail}"
 	source ./src/framework/interface_yes_no_box.sh
 	interface_yes_no help || exit 1
-	interface_yes_no "${1:-Are you sure?}" _process_yes_no
+	#interface_yes_no "${1:-Are you sure?}" _process_yes_no
 fi
