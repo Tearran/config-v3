4f53b426b29b9b2df9bd89003b528795e5a91752
tearran <tearran@gmail.com>
2025-06-11 03:09:12 +0000

    x style

--- templates/test_module_name_stub.sh
+++ templates/test_module_name_stub.sh
@@ -4,8 +4,8 @@
 set -euo pipefail
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    echo "WARNING: This is a stub test for test_checkpoint."
-    echo "Replace this stub with actual tests."
-    exit 1
+	echo "WARNING: This is a stub test for test_checkpoint."
+	echo "Replace this stub with actual tests."
+	exit 1
 fi
 
